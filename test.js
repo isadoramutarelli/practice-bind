0cd7a48a7cd61256b6884ce24ad7fbcede278477
isadoramutarelli <isadora.mutarelli@hotmail.com>
2024-07-12 18:53:28 +0100

    First commit

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-// Your code here
+
+const Employee = require("./class/employee.js");
+
+//create a new employee
+let johnWick = new Employee ("John Wick", "Dog Lover");
+
+//call sayName() after 2 seconds
+setTimeout(johnWick.sayName.bind(johnWick), 2000);
+
+//call sayOccupation() after 3 seconds
+setTimeout(johnWick.sayOccupation.bind(johnWick), 3000);
